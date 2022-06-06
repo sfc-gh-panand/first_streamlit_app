a213b2cf54b854c66c9390c1fbad10991e60f3b5
Anand Padmanabhan <anand.padmanabhan@snowflake.com>
2022-06-06 16:51:08 -0400

    filter the table data

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,14 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+
+#new section to dispaly fruityvice api response
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+streamlit.header('Fruityvice Fruit Advice')
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
