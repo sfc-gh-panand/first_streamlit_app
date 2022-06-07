925a6f21f5084c0520960baf9adefd0420bd269d
Anand Padmanabhan <anand.padmanabhan@snowflake.com>
2022-06-07 11:41:34 -0400

    filter the table data

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,4 +35,5 @@ fruityvice_normalized = pandas.json_normalize((fruityvice_response.json()))
 #output it the screen as the table
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
 
